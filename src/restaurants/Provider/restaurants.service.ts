b657afc984749e7bbc46ebcd0fb13f8534efea1b
Asif Newaz <asif.kravmaga@gmail.com>
2024-12-17 23:18:43 +0600

    Restaurant Specific review and change reservation

--- src/restaurants/Provider/restaurants.service.ts
+++ src/restaurants/Provider/restaurants.service.ts
@@ -4,6 +4,7 @@ import { Repository } from "typeorm";
 import { CreateRestaurantDto } from "../DTOs/create-restaurant.dto";
 import { User } from "src/users/user.entity";
 import { UpdateRestaurantDto } from "../DTOs/update-restaurant.dto";
+import { Reservation } from "src/reservations/reservation.entity";
 
 export class RestaurantsService {
     constructor(
@@ -11,6 +12,8 @@ export class RestaurantsService {
         private restaurantRepository: Repository<Restaurant>,
         @InjectRepository(User)
         private usersRepository: Repository<User>,
+        @InjectRepository(Reservation)
+        private reservationRepository: Repository<Reservation>,
     ) {}
 
     public async createRestaurant(createRestaurantDto: CreateRestaurantDto, userId: number){
@@ -28,6 +31,32 @@ export class RestaurantsService {
 
     public async getRestaurantById(id: number){
         return this.restaurantRepository.findOne({ where: { id } });
+    }
+
+    public async getRestaurantReviews(id: number) {
+        return this.restaurantRepository.findOne({
+            where: { id },
+            relations: ['reviews'], 
+        });
+    }
+
+    public async changeRestaurantReservation(id: number, reservationId: number, newStatus: string) {
+        
+        const restaurant = await this.restaurantRepository.findOne({
+            where: { id },
+            relations: ['reservations'],
+        });
+        if (!restaurant) {
+            throw new Error('Restaurant not found');
+        } 
+        const reservation = restaurant.reservations.find(
+            (res) => res.id === reservationId
+        );
+        if (!reservation) {
+            throw new Error('Reservation not found for this restaurant');
+        }
+        reservation.status = newStatus;
+        return await this.reservationRepository.save(reservation);
     }
 
     public async updateRestaurant(id: number, updateRestaurantDto: UpdateRestaurantDto){
